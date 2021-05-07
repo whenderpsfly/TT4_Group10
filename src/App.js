ed1e3b50bd33a777f61fba87b044fedc2ca10d84
Ming Xuan <mingxuan.toh@hotmail.com>
2021-05-07 12:03:04 +0800

    Login Gui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Username from './components/username';
+import Password from './components/password';
+import Login from './components/login';
+import Checkbox from './components/checkbox'
+import {Container} from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and weopopopo to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxWidth='xs' style={{backgroundColor:"skyblue"}}>
+        <Username></Username>
+        <Password></Password>
+        <Login></Login>
+        <Checkbox></Checkbox>
+      </Container>
     </div>
   );
 }
