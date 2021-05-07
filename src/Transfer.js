f02289f45bea5a0a31a1ada5a2ff324126c9d04a
Unknown <tiangpeiyuan@yahoo.com.sg>
2021-05-07 12:41:53 +0800

    Modify transfer barebones, checkbox not working

--- src/Transfer.js
+++ src/Transfer.js
@@ -1,22 +1,24 @@
-import React, { useState, useRef } from 'react'
+import React, { useRef, useState } from 'react'
+// import Checkbox from '@material-ui/core/Checkbox'
 
 export default function Transfer() {
     const transferIDNameRef = useRef()
     const transferAmtNameRef = useRef()
     const transferMsgNameRef = useRef()
-    // const [egift, setChecked] = React.useState(true)
+    // const [egift, seteGift] = useState(false)
+    // placeholder egift
     var egift = false
 
     function transferButton(e){
         const payeeID = transferIDNameRef.current.value
         const amount = transferAmtNameRef.current.value
-
+        const msg = transferMsgNameRef.current.value
         console.log("payeeID: " + payeeID)
-        console.log("amount:" + amount)
-
+        console.log("amount: " + amount)
+        console.log("message: " + msg)
+        // console.log("egift: " + egift)
         transferAmtNameRef.current.value = 0
     }
-    
 
     return (
         <>
@@ -28,12 +30,15 @@ export default function Transfer() {
             <input ref={transferMsgNameRef} type="text" />
             <div> 
                 eGift? 
-                <input type="checkbox" checked={egift} style = {{display : 'inline-block'}}/>
+                <input 
+                    type="checkbox" 
+                    checked={egift}
+                    //onClick={something}
+                    style = {{display : 'inline-block'}} 
+                />
             </div>
 
             <button onClick={transferButton}>Transfer</button>
-
         </>
-        
     )
 }
